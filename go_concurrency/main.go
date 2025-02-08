b98a6cf170100f2dec7c839de00ec35245c19e71
Chamuditha <lilan.2019660@iit.ac.lk>
2025-02-08 21:46:32 +0530

    initial main

---
+++ go_concurrency/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"go_currency/routes"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+)
+
+func main() {
+	app := fiber.New()
+
+	app.Use(cors.New(cors.Config{
+		AllowOrigins:     "http://localhost:4200",
+		AllowHeaders:     "Origin, Content-Type, Accept, Authorization",
+		AllowMethods:     "GET, POST, PUT, DELETE, OPTIONS",
+		AllowCredentials: true,
+	}))
+
+	routes.DataRoute(app)
+
+	app.Listen(":8080")
+}
